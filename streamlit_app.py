392bba9ba090f4b6687510cd2c48c81d434ab9fc
MinhVD <92356136+minhVoDuc@users.noreply.github.com>
2024-03-23 10:41:29 +0700

    update layout and content abstract

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,22 @@ import time
 
 # Title
 st.title('MS-VPR')
-st.divider()
+# st.divider()
+
+# Setting tabs
+st.markdown("""
+<style>
+	.stTabs [data-baseweb="tab"] {
+		border-radius: 4px 4px 0px 0px;
+		padding: 10px;
+    font-size: 2em;
+  }
+</style>""", unsafe_allow_html=True)
+tab_abstract, tab_eval, tab_interact, tab_demo = st.tabs(["ABSTRACT", "EVALUATION", "INTERACT", "DEMO"])
 
 # Sec 1. Abstract
-st.header('Abstract')
-content = open('./static/abstract.txt', 'r').read()
-# st.write(f"""{content}""")
-st.markdown(f'<div style="text-align: justify;">{content}</div>', unsafe_allow_html=True)
-st.image("./static/MultiMixVPR.jpg", caption="Model's Architecture")
+with tab_abstract:
+  content = open('./static/abstract.txt', 'r').read()
+  # st.write(f"""{content}""")
+  st.markdown(f'<div style="text-align: justify;">{content}</div><br>', unsafe_allow_html=True)
+  st.image("./static/MultiMixVPR.jpg", caption="Model's Architecture")
