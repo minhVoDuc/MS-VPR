47447f2f1e8e5d25252b2e8e1fdc456054ce07f4
MinhVD <92356136+minhVoDuc@users.noreply.github.com>
2024-03-24 12:05:23 +0700

    update interactive map

--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
 # MS-VPR
+
+## How to run local (on Windows)
+1. Create virtual env (venv)  
+`python -m venv .venv`
+2. Activate venv  
+`.venv\Scripts\activate`
+3. Install required libraries  
+`pip install -r requirements.txt`
+4. Prepage query images and place in `/static/query_map/`
+5. Run app  
+`streamlit run streamlit_app.py`
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from modules.interactive_map import create_map
+from modules.interactive_map import show_map
+
+st.set_page_config(layout="wide")
 
 # Title
 st.title('MS-VPR')
-# st.divider()
+st.markdown("""> **Made by:** Duc Quach-Minh, Minh Vo-Duc  
+            **Instructed by:** Mr. Anh Pham-Hoang""")
 
 # Setting tabs
 st.markdown("""
@@ -30,9 +33,18 @@ with tab_abstract:
 
 # Sec 3. Interact
 df = pd.read_csv('./static/gps.csv')
-r = create_map(df)
 
 with tab_interact:
-  # df
-  # st.map(data=df, size=1)
-  st.pydeck_chart(r)
+  col_1, col_2 = st.columns(2)  
+  with col_1:
+    mapbox_events = show_map(df)
+  
+  with col_2:
+    # Display the captured events
+    plot_name_holder_clicked = st.empty()
+    plot_name_holder_clicked.write("Please click a point on map")
+    if len(mapbox_events[0]) != 0:
+      img_name = str(df.loc[mapbox_events[0][0]['pointIndex'], 'timestamp']) + '.png'
+      plot_name_holder_clicked.write(f"Clicked Point: {img_name}")
+      # get image
+      st.image(f"./static/query_map/{img_name}", caption=img_name)
